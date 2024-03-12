942d3927ce122229118a2ea25279739bf57b6379
utopicat <cb.jaracontreras@gmail.com>
2024-03-12 19:15:56 -0300

    etiquetas indicadas paso a paso

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,7 @@
 
 //mensaje de form enviado//
-$(document).ready(function () {
-    $('#enviar-form').click(function () {
-        alert("El formulario de contacto fue enviado correctamente");
+$(document).ready(function () { //citar o convocar al documento//
+    $('#enviar-form').click(function () { //id enviar-form. evento hacer click//
+        alert("El formulario de contacto fue enviado correctamente"); //dar mensaje de alerta con el contenido indicado//
     });
-});
-
-//   function toggleMap() {
-//   var mapImage = document.getElementById("mapImage");
-// if (mapImage.style.display === "none") {
-//mapImage.style.display = "block";
-// } else {
-//mapImage.style.display = "none";
-//}
-//      }
+}); //cierres//
