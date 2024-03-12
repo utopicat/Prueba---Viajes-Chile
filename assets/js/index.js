31e8f074e2e2fdce1c4b18f3fba5db62de4fd61e
utopicat <cb.jaracontreras@gmail.com>
2024-03-12 18:23:07 -0300

    cambios html css y js

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+//mensaje de form enviado//
+$(document).ready(function () {
+    $('#enviar-form').click(function () {
+        alert("El formulario de contacto fue enviado correctamente");
+    });
+});
+
+//   function toggleMap() {
+//   var mapImage = document.getElementById("mapImage");
+// if (mapImage.style.display === "none") {
+//mapImage.style.display = "block";
+// } else {
+//mapImage.style.display = "none";
+//}
+//      }
